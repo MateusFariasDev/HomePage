7d85eefd7b0688ca0d1ed1b9a128eab057ad6014
Mateus Farias <mateus.f.silva@outlook.com>
2021-07-26 01:37:07 -0300

    Mudança no texto, quebra de linha em title e nova fonte

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -8,8 +8,10 @@ export const Home = () => {
   return (
     <div className="container">
       <div className="title">
-        <h1>Mateus Farias Silva</h1>
-        <small>Desenvolvedor Full Stack</small>
+        <h1>
+          <span className="block">Mateus</span> Farias Silva
+        </h1>
+        <small>Desenvolvedor Full Stack Júnior</small>
       </div>
       <div className="description">
         <p>
@@ -18,19 +20,18 @@ export const Home = () => {
         </p>
         <br />
         <p>
-          Para mais informações sobre meus projetos, acesse minhas redes sociais logo abaixo. Para informações de
-          contato, meu currículo está disponível no último ícone.
+          Para mais informações sobre meus projetos, redes sociais, contatos e currículo, acesse os ícones logo abaixo.
         </p>
       </div>
       <div className="medias">
         <a href="https://www.linkedin.com/in/coda/" target="_blank" title="Acesse meu LinkedIn" rel="noreferrer">
-          <FaLinkedin size={35} />
+          <FaLinkedin size={40} />
         </a>
         <a href="https://github.com/MateusFariasDev" target="_blank" title="Acesse meu Github" rel="noreferrer">
-          <FaGithub size={35} />
+          <FaGithub size={40} />
         </a>
         <a href={Curriculo} target="_blank" title="Acesse meu currículo" rel="noreferrer">
-          <FaFileDownload size={35} />
+          <FaFileDownload size={40} />
         </a>
       </div>
     </div>
